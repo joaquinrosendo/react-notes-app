59118c0f2d50ef5b8c89377e2db09c73b283182c
Joaquin Rosendo <joaquinrosendo@live.com>
2024-04-30 09:34:32 -0500

    add sort notes function

--- App.jsx
+++ App.jsx
@@ -15,6 +15,7 @@ export default function App() {
         notes.find(note => note.id === currentNoteId) 
         || notes[0]
 
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
 
     React.useEffect(() => {
         const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
@@ -38,7 +39,9 @@ export default function App() {
 
     async function createNewNote() {
         const newNote = {
-            body: "# Type your markdown note's title here"
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
         }
         const newNoteRef = await addDoc(notesCollection, newNote);
         setCurrentNoteId(newNoteRef.id)
@@ -46,7 +49,7 @@ export default function App() {
 
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId);
-        await setDoc(docRef, { body: text }, { merge: true });
+        await setDoc(docRef, { body: text, updatedAt: Date.now() }, { merge: true });
     }
 
     async function deleteNote(noteId) {
@@ -65,7 +68,7 @@ export default function App() {
                         className="split"
                     >
                         <Sidebar
-                            notes={notes}
+                            notes={sortedNotes}
                             currentNote={currentNote}
                             setCurrentNoteId={setCurrentNoteId}
                             newNote={createNewNote}
